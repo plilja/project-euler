2155c7757c991db0e61c7bcedd90a3997e8acd3e
plilja <patlil@kth.se>
2014-03-23 15:18:05 +0100

    name change

--- MultiplesOf3And5/multiples.py
+++ MultiplesOf3And5/multiples.py
@@ -1,8 +1,8 @@
 
 def multiples_of_x(limit_excl, x):
     limit_incl = limit_excl - 1
-    num_three_divisable = limit_incl / x
-    return int(x * (1.0 + num_three_divisable) * (num_three_divisable / 2.0))
+    num_x_divisable = limit_incl / x
+    return int(x * (1.0 + num_x_divisable) * (num_x_divisable / 2.0))
 
 def multiples_of_3_and_5(limit_excl):
     return multiples_of_x(limit_excl, 3) + multiples_of_x(limit_excl, 5) - multiples_of_x(limit_excl, 15)
