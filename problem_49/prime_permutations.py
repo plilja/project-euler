61df146d8ea91451f7a792c251a58b0df417b56d
plilja <patlil@kth.se>
2014-06-08 19:43:59 +0200

    naming

--- problem_49/prime_permutations.py
+++ problem_49/prime_permutations.py
@@ -9,14 +9,14 @@ SIEVE = PrimeSieve(10000)
 
 def prime_permutations():
     for digits in pick_from([1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 7, 7, 7, 8, 8, 9, 9, 9], 4):
-        prime_permutations = []
+        _prime_permutations = []
         for perm in permutations(digits):
             num = int(reduce(add, map(str, perm)))
             if num < 1000 or \
                     not SIEVE.is_prime(num):
                 continue
-            prime_permutations += [num]
-        combinations = pick_from(list(prime_permutations), 3)
+            _prime_permutations += [num]
+        combinations = pick_from(list(_prime_permutations), 3)
         for combination in combinations:
             if _fills_criteria(combination):
                 if 1487 in combination:
